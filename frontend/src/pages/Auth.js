e13f2f3825197ee835b2b16e53d27a5576e11d4c
Vijay <vijaykori202@gmail.com>
2026-04-13 05:50:37 +0000

    fixed api undefined

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -5,8 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { toast } from 'sonner';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const API = `${BACKEND_URL}/api`;
+const API = "https://digital-forensics-v.onrender.com/api";
 
 const Auth = ({ setToken }) => {
   const navigate = useNavigate();
